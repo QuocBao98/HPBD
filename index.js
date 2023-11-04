0cee94184cc6b84530d94ebda2e9427f14dae758
bao.tranphanquoc <tpqbao1998@gmail.com>
2023-11-04 23:55:50 +0700

    abc

---
+++ index.js
@@ -0,0 +1,17 @@
+if (document.location.search.match(/type=embed/gi)) {
+  window.parent.postMessage("resize", "*");
+}
+
+document.addEventListener("click", function () {
+  document.getElementById("player").play();
+});
+
+function showMessage() {
+  swal({
+    title: "Happy Birth Day",
+    text: "Happy Birth Day chị Dùm, chúc chị DÙm luôn vui vẻ, luôn xinh đẹp tươi tắn ",
+    button: {
+      text: "❤️️",
+    },
+  });
+}
